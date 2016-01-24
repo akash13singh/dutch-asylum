1e8c440a83e6f821523ff1e13f646d38b0cf5563
akash13singh <aakashsingh@gmail.com>
2016-01-24 14:11:34 +0100

    Code for choroplet map

--- js/map.js
+++ js/map.js
@@ -8,12 +8,18 @@ var zoom = d3.behavior.zoom()
 var width = document.getElementById('container').offsetWidth;
 var height = width / 2;
 
-var topo,projection,path,svg,g,asylum;
+var topo,projection,path,svg,g;
+var asylum = {};
 
 //var graticule = d3.geo.graticule();
 
 var tooltip = d3.select("#container").append("div").attr("class", "tooltip hidden");
 
+color = ["#D4B9DA","#C994C7","#DF65B0","#DD1C77","#980043"];
+var quantize = d3.scale.quantize()
+    .domain([0, 9000])
+    .range(d3.range(5).map(function(i) { return color[i] }));
+    
 setup(width,height);
 
 function setup(width,height){
@@ -41,11 +47,36 @@ queue().defer(d3.json, "data/world-topo-min.json")
 function ready(error,world,asylumRequests){
 	var countries = topojson.feature(world, world.objects.countries).features;
     topo = countries;
-    asylum = asylumRequests
-    console.log(asylum[0]);
-    console.log(asylum[1]);
-    console.log(asylum[2]);
-    console.log(asylum[3]);
+    
+    //load csv and build json
+    for (var i = 0; i < asylumRequests.length; i++) {
+        var obj = asylumRequests[i];
+        //console.log(obj);
+        if(asylum[obj["Country"]]== null){
+        	 tmp ={};
+        	 //TODO : remove later done to simplify json building
+        	 tmp['Citizenship'] = obj['Citizenship'];
+        	 tmp[2007] = {'Total':0,'M':{1:0,2:0},F:{1:0,2:0}};
+			 tmp[2008] = {'Total':0,'M':{1:0,2:0},F:{1:0,2:0}};
+			 tmp[2009] = {'Total':0,'M':{1:0,2:0},F:{1:0,2:0}};
+			 tmp[2010] = {'Total':0,'M':{1:0,2:0},F:{1:0,2:0}};
+			 tmp[2011] = {'Total':0,'M':{1:0,2:0},F:{1:0,2:0}};
+			 tmp[2012] = {'Total':0,'M':{1:0,2:0},F:{1:0,2:0}};
+			 tmp[2013] = {'Total':0,'M':{1:0,2:0},F:{1:0,2:0}};
+			 tmp[2014] = {'Total':0,'M':{1:0,2:0},F:{1:0,2:0}};
+        	 tmp[obj['Periods']]['Total'] = +obj['number'];  
+        	 tmp[obj['Periods']][obj['Sex']][obj['Age']] =  +obj['number'];  	
+        	 //console.log(JSON.stringify(tmp));
+        	 asylum[obj["Country"]]  = tmp;	
+        	//var tmp = {'Citizenship' : obj['Citizenship'], obj['Periods'] : { 'Total': obj['numbers'], obj['Sex'] : { obj['Age'] : obj['numbers'] }}};
+        	//asylum[obj["Country"]] = {'Citizenship' : obj['Citizenship'], obj['Periods'] :{ Total:obj['numbers'], obj['Sex']:{ obj['Age']:obj['numbers'] }}};       	
+        }
+       else{
+         tmp[obj['Periods']]['Total'] += +obj['number']; 
+         tmp[obj['Periods']][obj['Sex']][obj['Age']] +=  +obj['number']; 
+       } 
+    }   
+    //console.log(JSON.stringify(asylum));   
     draw(topo);
 }
     
@@ -72,13 +103,17 @@ function draw(topo) {
 
 
   var country = g.selectAll(".country").data(topo);
-
+  console.log(country);
   country.enter().insert("path")
       .attr("class", "country")
       .attr("d", path)
       .attr("id", function(d,i) { return d.id; })
       .attr("title", function(d,i) { return d.properties.name; })
-      .style("fill", function(d, i) { return d.properties.color; });
+      .style("fill", function(d, i) { if(asylum[d.properties.name]){
+      								    console.log(asylum[d.properties.name]['2007']['Total']);
+      								  	return quantize(asylum[d.properties.name]['2007']['Total']);
+      								  } 
+      								  return "#DDE7EB"; });
 
   //offsets for tooltips
   var offsetL = document.getElementById('container').offsetLeft+20;
